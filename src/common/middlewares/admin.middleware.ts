5fc314a8014e66fc6e47779b01fefc2f23b670b8
HelixW <shreyas.2000@hotmail.com>
2020-11-24 14:50:18 +0530

    feat: login validation added
    Signed-off-by: HelixW <shreyas.2000@hotmail.com>

--- src/common/middlewares/admin.middleware.ts
+++ src/common/middlewares/admin.middleware.ts
@@ -1,10 +1,15 @@
 import { NextFunction, Request, Response } from 'express'
+import { validDetails } from 'src/config/validation/admin.validator'
 
 export const verifyDetails = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  console.log(req.body)
-  next()
+  if (validDetails.validate(req.body).error)
+    res.status(400).json({
+      status: 'error',
+      message: 'incorrect email or password provided',
+    })
+  else next()
 }
---
+++ src/config/validation/admin.validator.ts
@@ -0,0 +1,15 @@
+import * as Joi from 'joi'
+
+export const validDetails = Joi.object({
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ['com', 'net'] },
+    })
+    .required(),
+  password: Joi.string()
+    .regex(
+      /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!#.])[A-Za-z\d$@$!%*?&.]{8,20}/
+    )
+    .required(),
+})
