847c6f8265162917dbf429e44e050ff990ac3e09
HelixW <shreyas.2000@hotmail.com>
2020-12-10 17:33:40 +0530

    feat(team-exists): added middleware for checking existing team
    Signed-off-by: HelixW <shreyas.2000@hotmail.com>

--- src/models/teams/teams.module.ts
+++ src/models/teams/teams.module.ts
@@ -1,6 +1,9 @@
 import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common'
 import { authorize } from 'src/shared/middlewares/admin.middleware'
-import { validateTeam } from 'src/shared/middlewares/teams.middleware'
+import {
+  checkExists,
+  validateTeam,
+} from 'src/shared/middlewares/teams.middleware'
 import { TeamsController } from './teams.controller'
 import { TeamsService } from './teams.service'
 
@@ -13,7 +16,7 @@ export class TeamsModule {
   configure(consumer: MiddlewareConsumer) {
     // validateAdmin and checkExists middlewares for register route
     consumer
-      .apply(validateTeam, authorize)
+      .apply(validateTeam, checkExists, authorize)
       .forRoutes({ path: 'teams/create', method: RequestMethod.POST })
   }
 }
--- src/shared/dto/auth.dto.ts
+++
@@ -1,3 +0,0 @@
-export class AuthHeader {
-  Authorization: string
-}
